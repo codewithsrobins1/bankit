837764fabc1361a0c321f4d5b798ae53478ccc48
unknown <codewithsrobins1@gmail.com>
2020-12-30 23:03:32 -0800

    prep for login auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { SignInPage } from './pages/SignInPage';
 import { LandingPage } from './pages/LandingPage';
 import { SignUpPage } from './pages/SignUpPage';
 import { Dashboard } from './pages/Dashboard';
+import { UserContext } from './context/UserContext';
 
 function App() {
+  // const [userData, setUserData ] = useState({}); 
+
   return (
     <Router>
-      <Switch>
-        <Route path="/" component={LandingPage} exact />
-        <Route path="/signin" component={SignInPage} exact />
-        <Route path="/signup" component={SignUpPage} exact />
-        <Route path="/dashboard" component={Dashboard} exact />
-      </Switch>
-
+      <UserContext.Provider value={{}}>
+        <Switch>
+          <Route path="/" component={LandingPage} exact />
+          <Route path="/signin" component={SignInPage} exact />
+          <Route path="/signup" component={SignUpPage} exact />
+          <Route path="/dashboard" component={Dashboard} exact />
+        </Switch>
+      </UserContext.Provider>
     </Router>
   );
 }
