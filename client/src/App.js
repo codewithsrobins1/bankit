30b7c43ba312cedc7479f86f6fc6d1d849aa62a8
unknown <codewithsrobins1@gmail.com>
2021-01-12 18:28:12 -0800

    removed context api boiler

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { SignInPage } from './pages/SignInPage';
 import { LandingPage } from './pages/LandingPage';
 import { SignUpPage } from './pages/SignUpPage';
 import { Dashboard } from './pages/Dashboard';
-import { UserContext } from './context/UserContext';
 
 function App() {
-  // const [userData, setUserData ] = useState({}); 
-
   return (
     <Router>
-      <UserContext.Provider value={{}}>
         <Switch>
           <Route path="/" component={LandingPage} exact />
           <Route path="/signin" component={SignInPage} exact />
           <Route path="/signup" component={SignUpPage} exact />
           <Route path="/dashboard" component={Dashboard} exact />
         </Switch>
-      </UserContext.Provider>
     </Router>
   );
 }
